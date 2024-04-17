786117b43c56be432f75f46209c40aa057ccbbe3
NoamSeb <noam.sebahoun@gmail.com>
2024-04-17 20:59:37 +0200

    :sparkles: Translation using i18n :sparkles: Testimonials from translation file

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,13 +1,48 @@
 import Link from "next/link";
 import { useTranslation } from "../i18n";
+import styles from "./page.module.scss";
+import TopBar from "@/components/topBar/TopBar";
+import NavBar from "@/components/navBar/NavBar";
+import Testimonials from "@/components/testimonials/Testimonials";
 
 export default async function Page({ params: { lng } }) {
   const { t } = await useTranslation(lng);
 
   return (
     <>
-      <h1>{t("whoAmI")}</h1>
-      <Link href={`/${lng}/second-page`}>second page</Link>
+      <header>
+        <TopBar />
+      </header>
+      <NavBar lng={lng} />
+      <main>
+        <div className={styles.titleSection}>
+          <h1>
+            <span className={styles.r}>{t("myName")}</span>
+            <br></br>
+            <span className={styles.b}>{t("mySurname")}</span>
+          </h1>
+          <img src="./face-picture.webp" alt="" />
+        </div>
+        <div id="whoAmI" className={styles.whoAmI}>
+          <h2 className={styles.b}>{t("whoAmI.title")}</h2>{" "}
+          <div>
+            {[1, 2, 3, 4].map((i) => (
+              <p>{t("whoAmI.content" + i + "")}</p>
+            ))}
+            <img src="" alt="" />
+          </div>
+        </div>
+        <div id="whatIDO" className={styles.whatIDO}>
+          <h2 className={styles.b}>{t("whatIDO.title")}</h2>{" "}
+        </div>
+        <div id="testimonials" className={styles.testimonials}>
+          <h2 className={styles.b}>{t("testimonials.title")}</h2>{" "}
+          <Testimonials lng={lng} />
+        </div>
+        <div id="contact" className={styles.contact}>
+          <h2 className={styles.b}>{t("contact.title")}</h2>{" "}
+        </div>
+      </main>
     </>
   );
 }
