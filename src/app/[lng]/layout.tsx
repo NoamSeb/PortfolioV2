8605a585719f0c49ee7bd6e7c25a2e24f082c085
NoamSeb <noam.sebahoun@gmail.com>
2024-05-04 17:40:42 +0200

    :spakles: add pictures for projects

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -1,11 +1,22 @@
+import React from "react";
 import { dir } from "i18next";
 import { languages } from "../i18n/settings";
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+  params: {
+    lng: string;
+  };
+}
 
 export async function generateStaticParams() {
   return languages.map((lng) => ({ lng }));
 }
 
-export default function RootLayout({ children, params: { lng } }) {
+export default function RootLayout({
+  children,
+  params: { lng },
+}: RootLayoutProps) {
   return (
     <html lang={lng} dir={dir(lng)}>
       <head />
