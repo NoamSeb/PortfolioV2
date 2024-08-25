e84a596716def8bdab6e906eb33ef484dbf3fa87
NoamSeb <noam.sebahoun@gmail.com>
2024-08-25 20:34:49 +0200

    :sparkles: add WebOdyssey Project

--- src/components/topBar/TopBar.tsx
+++ src/components/topBar/TopBar.tsx
@@ -1,8 +1,13 @@
+"use client";
 import styles from "./topBar.module.scss";
 import Link from "next/link";
 import Logo from "../logo/Logo";
+import { useParams } from "next/navigation";
 
 export default function TopBar() {
+  const router = useParams();
+
+  console.log(router);
   return (
     <div className={styles.topBar}>
       <div>
@@ -17,7 +22,8 @@ export default function TopBar() {
           <img src="./moon.svg" alt="" className={styles.moon} />
         </label>
         <p>
-          <Link href="./en">EN</Link> | <Link href="./fr">FR</Link>
+          <Link href={`/en/${router.slug}`}>EN</Link> |{" "}
+          <Link href={`/fr/${router.slug}`}>FR</Link>
         </p>
       </div>
     </div>
