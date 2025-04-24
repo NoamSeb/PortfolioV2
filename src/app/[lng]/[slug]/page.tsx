362341036ad7a88f72ecef5e3f0f44e8e5f4f039
noamSeb <noam.sebahoun@gmail.com>
2025-04-24 12:15:44 +0200

    update page project

--- src/app/[lng]/[slug]/page.tsx
+++ src/app/[lng]/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { useTranslation } from "../../i18n";
 import styles from "./project.module.scss";
-import { Metadata } from 'next';
+import { Metadata } from "next";
 
 import TopBar from "@/components/topBar/TopBar";
 import NavBar from "@/components/navBar/NavBar";
@@ -13,7 +13,20 @@ interface ProjectProps {
   };
 }
 
-export async function generateMetadata({ params }: ProjectProps): Promise<Metadata> {
+interface WorkItem {
+  task: string;
+  descTask: string;
+  mediaTask: string;
+}
+
+interface Project {
+  MyWorkTitle: string;
+  Work?: WorkItem[];
+}
+
+export async function generateMetadata({
+  params,
+}: ProjectProps): Promise<Metadata> {
   const { slug } = params;
   return {
     title: `Project | ${slug}`,
@@ -42,7 +55,10 @@ export default async function Page({
       <header>
         <TopBar />
         <div className={styles.titleSection}>
-          <h1 className={styles.b}>{project.name}</h1>
+          <div className={styles.titleSectionText}>
+            <h1 className={styles.b}>{project.myTasks}</h1>
+            <h2 className={`${styles.b} ${styles.xpTitle}`}>{project.name}</h2>
+          </div>
           <img src={project.image} alt={project.name} />
         </div>
       </header>
@@ -65,7 +81,10 @@ export default async function Page({
               </p>
               <h2 className={styles.b}>{t(project.description.title)}</h2>
               <p className={styles.r}>{project.description.content}</p>
-              <Link href={`${project.externalLink.link}`} text-area={`See ${project.name} Project`}>
+              <Link
+                href={`${project.externalLink.link}`}
+                text-area={`See ${project.name} Project`}
+              >
                 <button
                   className={`${styles.projectInfosDescriptionExternalLink} ${styles.b}`}
                 >
@@ -74,11 +93,28 @@ export default async function Page({
               </Link>
             </div>
           </div>
-            <div className={styles.myWorkOnProject}>
-                <h2 className={styles.b}>{t(project.MyWorkTitle)}</h2>
+          <div className={styles.myWorkOnProject}>
+            <h2 className={styles.b}>{t(project.MyWorkTitle)}</h2>
+            <div className={styles.work}>
+              {project.Work?.map((item: WorkItem, tagIndex: number) => (
+                <div key={tagIndex} className={`${styles.workDetail} ${tagIndex % 2 === 0 ? '' : styles.rowReverse}`}>
+                  <div className={styles.workDetailText}>
+                  <h3 className={`${styles.b} ${styles.TaskTitle}`}>{item.task}</h3>
+                  <p className={`${styles.r} ${styles.TaskDesc}`}>{item.descTask}</p>
+                  </div>
+                  {item.mediaTask && (
+                    <img
+                      src={item.mediaTask}
+                      alt={item.task}
+                      className={styles.TaskMedia}
+                    />
+                  )}
+                </div>
+              ))}
             </div>
+          </div>
         </div>
-        <NavBar lng={lng}/>
+        <NavBar lng={lng} />
       </main>
     </>
   );
